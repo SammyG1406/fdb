d758ee900ab61b3d00b0c892897a1523eccddde1
SammyG69 <samarthgupta14@yahoo.com.sg>
2026-05-20 21:43:38 +1000

    Adding more routes in the proxy.ts for them to be valid

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -4,6 +4,8 @@ const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
   '/sign-up(.*)',
   '/api/foods(.*)',
+  '/api/meals(.*)',
+  '/api/users(.*)'
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
